ce62e7a14fb8f7bad2d397d466545c00494b889f
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-20 11:04:30 +0530

    Create java if else statement.java

---
+++ java if else statement.java
@@ -0,0 +1,14 @@
+package p1;
+class test 
+{
+	public static void main(String[] args)
+	{
+		int i = 555;
+		int j = 444;
+		if(i==j){
+			System.out.println("the i and j values are equal");
+		}else{
+			System.out.println("the i and j values are not equal");
+		}
+	}
+}
