70d7299cfd6c98916b48eb2b8e2a0cb15a2150fd
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-20 10:52:39 +0530

    Create doubt int print.java

---
+++ doubt int print.java
@@ -0,0 +1,10 @@
+package p1;
+class test {
+	public static void main(String[] args){
+	int i = 23;	
+	float f = 34.f;
+	System.out.println("the integer value is:",i);
+	System.out.println("the float valus is:" f);
+	}
+	
+}
