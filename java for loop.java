8b78b0f0de456be6d183a168cba153df2980cdbb
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-20 12:54:02 +0530

    Create java for loop.java

---
+++ java for loop.java
@@ -0,0 +1,24 @@
+//ex:1
+package p1;
+class test
+{
+	public static void main(String[] args)
+	{
+		for(int i=0;i<5;i++){
+			System.out.println("haii anna");
+		}
+	}
+}
+
+
+//ex:2
+package p1;
+class test
+{
+	public static void main(String[] args)
+	{
+		for(int i=0,j=0;i<8&&j<8;i++,j++){
+			System.out.println(i+ " "+j);
+		}
+	}
+}
