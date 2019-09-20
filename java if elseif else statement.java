6df77e1a47fe577faee0ec1dae9b68c7029cb5cf
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-20 11:23:18 +0530

    Create java if elseif else statement.java

---
+++ java if elseif else statement.java
@@ -0,0 +1,14 @@
+package p1;
+class test
+{
+	public static void main(String[] args){
+		int i =344;
+		if(i==233){
+			System.out.println("i is equal to 233");
+		}else if(i>500){
+			System.out.println("i is greater than 500");
+		}else{
+			System.out.println("the i value is 344");
+		}
+	}
+}
