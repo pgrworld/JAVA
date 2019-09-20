1aaf5e0249592d7a32c70796183364e39f4ac540
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-20 13:09:33 +0530

    Create java while loop.java

---
+++ java while loop.java
@@ -0,0 +1,13 @@
+//if we are not aware of no.of iterations 
+package p1;
+class test
+{
+	public static void main(String[] args)
+	{
+		int i =0;
+		while(i<5){
+			System.out.println("haii mom");
+		    i++;
+		}
+	}
+}
