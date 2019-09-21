2413cab6d2c2bf955b4229f3a5bfbbc0693b4547
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-21 16:35:53 +0530

    Create java extends.java

---
+++ java extends.java
@@ -0,0 +1,16 @@
+package pentakota;
+class B extends A
+{
+	public static void main(String[] args)
+	{
+		System.out.println(A.i);
+		System.out.println(A.str);
+	}
+}
+
+class A
+{
+	 static int i = 89;
+	 static String str = "haii sindhu";
+	
+}
