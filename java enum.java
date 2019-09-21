0636be9cf19107ea4ba425eecc5d3ba9c9a51979
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-21 17:03:11 +0530

    Create java enum.java

---
+++ java enum.java
@@ -0,0 +1,12 @@
+package pentakota;
+enum User{
+	GOWRAV,RAHUL,PENTAKOTA;
+}
+class Test{
+	public static void main(String[] args)
+	{
+		System.out.println(User.GOWRAV);
+		System.out.println(User.RAHUL);
+		System.out.println(User.PENTAKOTA);
+	}
+}
