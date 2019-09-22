86536d4b67f2b233803ef0e0ecdb0f570f9960ac
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-22 19:49:33 +0530

    Create java instanceof.java

---
+++ java instanceof.java
@@ -0,0 +1,25 @@
+package pentakota;
+class A
+{
+	
+}
+class B extends A
+{
+	
+}
+class c
+{
+	
+}
+class Test
+{
+	public static void main(String[] args)
+	{
+	
+		A a = new A();
+		B b = new B();
+   System.out.println(a instanceof A);
+   System.out.println(b instanceof A);
+   
+	}
+}
