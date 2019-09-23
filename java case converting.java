a4d40a53bf662e74d5fc8686777dfe718913ad0c
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-23 16:30:52 +0530

    Create java case converting.java

---
+++ java case converting.java
@@ -0,0 +1,10 @@
+package training;
+class Test
+{
+	public static void main(String[] args)
+	{
+		String str = "    HAII ANNA    ";
+		System.out.println(str.toLowerCase());
+		System.out.println(str.toUpperCase());
+	}
+}
