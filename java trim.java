7b1969b62fc00de081cafae333989b02318be29e
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-23 16:29:31 +0530

    Create java trim.java

---
+++ java trim.java
@@ -0,0 +1,10 @@
+// FOR AVOIDING SPACE STARTING AND ENDING 
+package training;
+class Test
+{
+	public static void main(String[] args)
+	{
+		String str = "      haii anna    ";
+		System.out.println(str.trim());
+	}
+}
