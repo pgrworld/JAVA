94b26aa4cc837667a531917f5419d48b38ebeb39
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-24 15:55:58 +0530

    Create java collection sorting order.java

---
+++ java collection sorting order.java
@@ -0,0 +1,26 @@
+package training;
+
+//import java.util.ArrayList;
+import java.util.TreeSet;
+
+class Test
+{
+	public static void main(String[] args)
+	{
+//		Test[] std = new Test[3];
+//		std[0] = new Test();
+//		std[1] = new Test();
+//		System.out.println(std);
+		
+		TreeSet ts = new TreeSet();
+		ts.add(3);
+		ts.add(2);
+		System.out.println(ts);
+		
+	}
+}
+ 
+
+
+
+
