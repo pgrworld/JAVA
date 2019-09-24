b9ae1acfe941e87ebdedd8e824e0525803b9274f
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-24 16:23:12 +0530

    Create java hastset.java

---
+++ java hastset.java
@@ -0,0 +1,14 @@
+package training;
+import java.util.*;
+class Test
+{
+	public static void main(String[] args)
+	{
+	HashSet hs = new HashSet();
+	System.out.println(hs.add("a"));
+	hs.add("b");
+	hs.add("c");
+	System.out.println(hs);
+	
+	}
+}
