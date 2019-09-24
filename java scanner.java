78adfb0edb97db2d4e1d78a7cbcfb7261b7f969a
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-09-24 13:13:22 +0530

    Create java scanner.java

---
+++ java scanner.java
@@ -0,0 +1,16 @@
+package training;
+import java.util.*;
+class Test
+{
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Employee Number");
+		int eno = s.nextInt();
+		System.out.println("Employee Name");
+		String ename = s.next();
+		
+		System.out.println(eno);
+		System.out.println(ename);
+	}
+}
